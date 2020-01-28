b79cf1fa6460daf621c43458a50d7042ac83e03e
Sarah Deakin <sarah.deakin@starlingbank.com>
2020-01-28 15:42:39 +0000

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Julia Doe`,
-    subtitle: `software developer`,
+    title: `Hi Elanor`,
+    subtitle: `I love you`,
     description: `A minimal blog starter built with Gatsbyjs. The needed Gatsby files are included.`,
-    author: `@niklasmtj`,
+    author: `@sdeakin93`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
